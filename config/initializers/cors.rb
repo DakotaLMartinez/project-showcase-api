206dad4f1a912aacac2af43e08cceba285cf08c7
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-02-10 17:53:00 -0800

    update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173", "https://developer-project-showcase.netlify.app/"
 
     resource "*",
       headers: :any,
-      expose: ["Authorization"],
+      expose: ["Authorization", "Access-Control"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
