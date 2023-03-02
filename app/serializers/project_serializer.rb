7b120c003e81e32d17d51057ab026c9e2ade8291
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-03-02 14:50:06 -0800

    add user_id to serialized projects

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,6 +1,6 @@
 class ProjectSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :about, :live_demo_url, :code_url, :video_url, :technologies, :collaborators
+  attributes :id, :name, :about, :live_demo_url, :code_url, :video_url, :technologies, :collaborators, :user_id
 
   attribute :featured_image_url do |project|
     Rails.application.routes.url_helpers.rails_blob_url(project.featured_image) if project.featured_image.attached?
