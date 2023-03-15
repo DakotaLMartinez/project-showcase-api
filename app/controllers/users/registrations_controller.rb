c4c73933054162278b5db5b622cfe278af4c437c
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-03-15 16:16:36 -0700

    handle delete account response with 200 status

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -64,11 +64,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def respond_with(resource, _opts = {})
-    if resource.persisted?
+    if request.method == "POST" && resource.persisted?
       render json: {
-        status: {code: 200, message: 'Signed up sucessfully.'},
+        status: {code: 200, message: "Signed up sucessfully."},
         data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-      }
+      }, status: :ok
+    elsif request.method == "DELETE"
+      render json: {
+        status: { code: 200, message: "Account deleted successfully."}
+      }, status: :ok
     else
       render json: {
         status: {code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
